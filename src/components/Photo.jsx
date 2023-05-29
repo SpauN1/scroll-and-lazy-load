cca1efe593c5c27b38fa9906bde24105ec763292
Spaun1 <andreispaun7@gmail.com>
2023-05-29 22:50:02 +0300

    use intersection-observer hook

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import { useInView } from 'react-intersection-observer';
 
-function Photo({ imageUrl, name }) {
+const Photo = ({ imageUrl, name }) => {
+  const { ref, inView } = useInView({
+    threshold: 0.5,
+    triggerOnce: true,
+  });
+
   return (
-    <div className="photo-card">
-      <img src={imageUrl} alt="nature" />
+    <div ref={ref} className="photo-card">
+      {inView ? (
+        <img src={imageUrl} alt="nature" />
+      ) : (
+        <div className="photo-card__skeleton" />
+      )}
+
       <h4>{name}</h4>
     </div>
   );
-}
+};
 
 export default Photo;
